8046a2432867f38cc08be2110e7c437a0917d364
Mansi-weboappdiscovery <134059197+Mansi-weboappdiscovery@users.noreply.github.com>
2023-06-19 17:08:52 +0530

    updated settings.py (#19)

--- property/settings.py
+++ property/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'drf_spectacular',
+    'corsheaders',
     # new applications.
     'foundation',
     'tasks',
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
