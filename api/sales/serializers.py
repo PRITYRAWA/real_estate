5add60fe97c3500d827482cc9c841e74f48a72c4
GurleenWeboApp <123358747+GurleenWeboApp@users.noreply.github.com>
2023-07-06 12:05:20 +0530

    fixed (#57)
    * modelchanges
    
    * sales-tenders fixed
    
    ---------
    
    Co-authored-by: GurleenWeboApp <vedanshullr@gmail.com>

--- api/sales/serializers.py
+++ api/sales/serializers.py
@@ -9,8 +9,26 @@ class PersonmoveinSerializer(serializers.ModelSerializer):
         exclude = ('created_at', 'updated_at')
 
 class TenderSerializer(serializers.ModelSerializer):
-    property = serializers.CharField(read_only=True, source='property.name')
-    object = serializers.CharField(read_only=True, source='object.object_name')
+    property = serializers.CharField(source='property.name')
+    object = serializers.CharField(source='object.object_name')
+
     class Meta:
         model = Tender
         exclude = ('created_at', 'updated_at')
+    
+    def create(self, validated_data):
+        property_data = validated_data.pop('property', None)
+        object_data = validated_data.pop('object', None)
+
+        tender = Tender.objects.create(**validated_data)
+
+        if property_data:
+            property_instance, _ = Realestateproperties.objects.get_or_create(name=property_data['name'])
+            tender.property = property_instance
+
+        if object_data:
+            object_instance, _ = Realestateobjects.objects.get_or_create(object_name=object_data['object_name'])
+            tender.object = object_instance
+
+        tender.save()
+        return tender
